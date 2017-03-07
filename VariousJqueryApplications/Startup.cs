ce617833865a2304058c6516cd6c0eab578df2c8
bsokat <barissokat@gmail.com>
2017-03-07 09:24:33 +0300

    Initial commit

---
+++ VariousJqueryApplications/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VariousJqueryApplications.Startup))]
+namespace VariousJqueryApplications
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
